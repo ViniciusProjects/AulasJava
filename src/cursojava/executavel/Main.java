2a55ef95a8c6d46bf3d3283681d592da8cfe4591
vinicius <vinisilva2001@hotmail.com>
2020-06-08 15:17:53 -0300

    Procurando aluno e calculando média

--- src/cursojava/executavel/Main.java
+++ src/cursojava/executavel/Main.java
@@ -1,5 +1,8 @@
 package cursojava.executavel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 import cursojava.classes.Aluno;
@@ -9,60 +12,60 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		String nome = JOptionPane.showInputDialog("Nome do aluno");
-		String idade = JOptionPane.showInputDialog("idade do aluno");
-		String cpf = JOptionPane.showInputDialog("CPF do aluno");
+		List<Aluno> alunos = new ArrayList<Aluno>();
 
-		Aluno aluno1 = new Aluno();
+		for (int qtd = 1; qtd <= 2; qtd++) {
 
-		aluno1.setNome(nome);
-		aluno1.setIdade(Integer.valueOf(idade));
-		aluno1.setNumeroCpf(cpf);
+			String nome = JOptionPane.showInputDialog("Nome do aluno " + qtd + " ?");
+			String idade = JOptionPane.showInputDialog("idade do aluno");
+			String cpf = JOptionPane.showInputDialog("CPF do aluno");
 
-		for (int i = 1; i <= 4; i++) {
-			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + i);
-			String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + i);
+			Aluno aluno1 = new Aluno();
 
-			Disciplina disciplina = new Disciplina();
-			disciplina.setDisciplina(nomeDisciplina);
-			disciplina.setNota(Double.valueOf(notaDisciplina));
+			aluno1.setNome(nome);
+			aluno1.setIdade(Integer.valueOf(idade));
+			aluno1.setNumeroCpf(cpf);
 
-			aluno1.getDisciplinas().add(disciplina);
-		}
+			for (int i = 1; i <= 4; i++) {
+				String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + i);
+				String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + i);
 
-		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina? ");
-		if (escolha == 0) { // opção sim é zero
-			int continuarRemover = 0;
-			int posicao = 1;
-			while (continuarRemover == 0) {
-				String disciplinaRemover = JOptionPane.showInputDialog("Qual disciplina 1, 2, 3, 4 ? ");
-				aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
-				posicao++;
-				continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover?");
+				Disciplina disciplina = new Disciplina();
+				disciplina.setDisciplina(nomeDisciplina);
+				disciplina.setNota(Double.valueOf(notaDisciplina));
 
+				aluno1.getDisciplinas().add(disciplina);
 			}
 
+			int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina? ");
+			if (escolha == 0) { // opção sim é zero
+				int continuarRemover = 0;
+				int posicao = 1;
+				while (continuarRemover == 0) {
+					String disciplinaRemover = JOptionPane.showInputDialog("Qual disciplina 1, 2, 3, 4 ? ");
+					aluno1.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue() - posicao);
+					posicao++;
+					continuarRemover = JOptionPane.showConfirmDialog(null, "Continuar a remover?");
+
+				}
+			}
+			alunos.add(aluno1);
 		}
-
-		System.out.println(aluno1);
-		System.out.println("Nome: " + aluno1.getNome());
-		System.out.println("Idade: " + aluno1.getIdade());
-		System.out.println("CPF: " + aluno1.getNumeroCpf());
-		System.out.println("Média da nota: " + aluno1.getMediaNota());
-		System.out.println("Disciplinas listadas: " + aluno1.getDisciplinas().size());
-		System.out.println("Nome das disciplinas listadas: " + aluno1.getDisciplinas());
-		System.out.println("Resultado: " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-		System.out.println("Resultado final: " + aluno1.getAlunoAprovado2());
-
-		Aluno a2 = new Aluno();
-		String cpfAluno2 = JOptionPane.showInputDialog("Digite seu cpf");
-		a2.setNumeroCpf(cpfAluno2);
-
-		if (aluno1.equals(a2)) { // verificando se o cpf setado é de um outro aluno
-			System.out.println("CPF pertence a outro aluno");
-
+		for (Aluno aluno : alunos) {
+			if (aluno.getNome().equalsIgnoreCase("Vinicius")) { //procurando aluno e calculando média
+				System.out.println(aluno);
+				System.out.println("Nome: " + aluno.getNome());
+				System.out.println("Idade: " + aluno.getIdade());
+				System.out.println("CPF: " + aluno.getNumeroCpf());
+				System.out.println("Média da nota: " + aluno.getMediaNota());
+				System.out.println("Disciplinas listadas: " + aluno.getDisciplinas().size());
+				System.out.println("Nome das disciplinas listadas: " + aluno.getDisciplinas());
+				//System.out.println("Resultado: " + (aluno.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+				System.out.println("Resultado final: " + aluno.getAlunoAprovado2());
+				System.out.println("----------------------------------------------------------------------------");
+				break;
+			}
 		}
 
 	}
-
 }
