debe458f5f55710ea0e50d962be53b93ea0e62a5
vinicius <vinisilva2001@hotmail.com>
2020-07-09 21:05:37 -0300

    Declarando os métodos e a constante como static e usei diretamente o nome da classe no main

--- src/br/com/udemy/cursojava/application/Radius.java
+++ src/br/com/udemy/cursojava/application/Radius.java
@@ -13,16 +13,14 @@ public class Radius { //2° versão
 		System.out.println("Enter radius");
 		Scanner sc = new Scanner(System.in);
 		double radius = sc.nextDouble();
-
-		Calculator calc = new Calculator ();
 		
-		double c = calc.circumference(radius);
-		double v = calc.volume(radius);
+		double c = Calculator.circumference(radius);
+		double v = Calculator.volume(radius);
 
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
 		
-		System.out.printf("PI value: %.2f%n", calc.PI);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
 
 		sc.close();
 	}
