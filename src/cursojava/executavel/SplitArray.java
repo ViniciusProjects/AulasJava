ce9f5489093bba41caccc935e17a2e58372fa53e
vinicius <vinisilva2001@hotmail.com>
2020-07-15 13:33:42 -0300

    Trabalhando com arrays

--- src/cursojava/executavel/SplitArray.java
+++ src/cursojava/executavel/SplitArray.java
@@ -1,5 +1,8 @@
 package cursojava.executavel;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SplitArray {
 
 	public static void main(String[] args) {
@@ -8,11 +11,26 @@ public class SplitArray {
 		String[] array = text.split(",");
 		System.out.println("Name:" + array[0]);
 		System.out.println("Course:" + array[1]);
-		System.out.println("Grade:" 	+ array[2]);
+		System.out.println("Grade:" + array[2]);
 		System.out.println("Grade:" + array[3]);
 		System.out.println("Grade:" + array[4]);
 		System.out.println("Grade: " + array[5]);
 
+		System.out.println("------------------------------------------------------------------");
+		// Converting an array to a list
+		List<String> list = Arrays.asList(array);
+
+		for (String valueString : list) {
+			System.out.println(valueString);
+
+		}
+		System.out.println("------------------------------------------------------------------");
+		// Converting a list to an array//
+		String[] ConvertArray = list.toArray(new String[6]);
+		for (int i = 0; i < ConvertArray.length; i++) {
+			System.out.println(ConvertArray[i]);
+		
+		}
 	}
 
 }
